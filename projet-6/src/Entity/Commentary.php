6f0645f82da30bc9ba2e907933f2f1c8c4c20d78
Alexandre-Errecade <alexandre.errecade@tut-tut.com>
2023-02-07 13:30:28 +0100

    #2 database

---
+++ projet-6/src/Entity/Commentary.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentaryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentaryRepository::class)
+ */
+class Commentary
+{
+
+    use \App\Traits\ActivableBoolean;
+    use \App\Traits\Timestampable;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="commentaries")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Figure::class, inversedBy="commentaries")
+     */
+    private $figure;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->user;
+    }
+
+    public function setUser(?user $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getFigure(): ?figure
+    {
+        return $this->figure;
+    }
+
+    public function setFigure(?figure $figure): self
+    {
+        $this->figure = $figure;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
